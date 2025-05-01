b141e92e2a1fb3bf986120a6892ab3e649472d51
d <g>
2025-05-01 11:17:40 -0500

    meow

--- pre_proccess.py
+++ pre_proccess.py
@@ -1,8 +1,4 @@
 ﻿import pandas as pd
-from sympy import symbols, sympify, Mul
-from sympy.parsing.sympy_parser import parse_expr
-from sympy.core.sympify import SympifyError
-from tokenize import TokenError 
 import re
 
 
@@ -14,6 +10,7 @@ class Expression:
 		self.raw = expression
 		self.raw_terms = self.extract_terms(expression)
 		self.term_factors = {}
+		self.next_var = 48
 		self.reversed_term_factors = {}
 		self.parsed_terms = [self.parse_term(term) for term in self.raw_terms]
 
@@ -29,32 +26,53 @@ class Expression:
 		for k, v in self.reversed_term_factors.items():
 			out.append(f"  {k}: {v}")
 		return "\n".join(out)
+	
+	def extract_terms(self, expression):
+		return expression.split(" ")
+	
+	def extract_factors(self, term):
+		algebreic = re.sub(r'-?\d+', '', term)
+		nums = [float(num) for num in re.findall(r'-?\d+(?:\.\d+)?', term)]
+		return (algebreic, nums)
 
-	def parse_term(self, term_raw):
+	def parse_term(self, term):
 		parsed_term = []
-		term = self.safe_parse_expr(term_raw)
-		factors = Mul.make_args(term)
-		next_var = 48
+		text_factors, numeric_factors = self.extract_factors(term)
 
+		factors = list(text_factors) + numeric_factors
 
 		for factor in factors:
 			factor_str = str(factor)
 
 			if factor_str not in self.term_factors.values():
-				self.term_factors[chr(next_var)] = factor_str
-				self.reversed_term_factors[factor_str] = chr(next_var)
+				self.term_factors[chr(self.next_var)] = factor_str
+				self.reversed_term_factors[factor_str] = chr(self.next_var)
 
-				parsed_term.append(chr(next_var))
+				parsed_term.append(chr(self.next_var))
 
-				next_var += 1
+				self.next_var += 1
 			else:
 				parsed_term.append(self.reversed_term_factors[factor_str])
 
 		return parsed_term
+	
+	def get_common_elements(list1, list2):
+		return list(set(list1) & set(list2))
+
+	def compare(self, expression):
+		common_factors = self.get_common_elements(expression.term_factors.values(), self.term_factors.values())
+		num_common_factors = len(common_factors)
+
+		common_terms = self.get_common_elements(self.raw_terms, expression.raw_terms)
+		num_common_terms = len(common_terms)
+
+		
+		return [num_common_factors, num_common_terms]
 
 
-df["parsed_options"] = [[Expression(answer[2:]) for answer in answers] for answers in df["options"]]
+parsed_answers = [[Expression(answer[2:]) for answer in answers] for answers in df["options"]]
 
-for items in df["parsed_options"]:
-	for item in items:
-		print(item)
+
+answers = ["A", "B", "C", "D", "E"]
+
+prcs_df = pd.DataFrame()
